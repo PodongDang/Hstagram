6ac98bc7136b8d80150f3e84c186364458d692ab
Local_Hwang <amongzamong@gmail.com>
2024-12-30 15:59:19 +0900

    feat : Add redis and session

--- Hstagram/src/main/java/SNS/Hstagram/SecurityConfig.java
+++ Hstagram/src/main/java/SNS/Hstagram/SecurityConfig.java
@@ -2,8 +2,10 @@ package SNS.Hstagram;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +14,5 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
---
+++ Hstagram/src/main/java/SNS/Hstagram/interceptor/AuthInterceptor.java
@@ -0,0 +1,63 @@
+package SNS.Hstagram.interceptor;
+
+import SNS.Hstagram.exception.AuthException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.Base64;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private static final String SESSION_KEY = "SESSION";
+    private static final String REDIS_SESSION_KEY = ":sessions:";
+
+    @Value("${spring.session.redis.namespace}")
+    private String namespace;
+
+    private final StringRedisTemplate redisTemplate;
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request,
+                             final HttpServletResponse response,
+                             final Object handler) throws Exception {
+
+        // 1. 쿠키에서 세션 ID 추출
+        final String sessionIdByCookie = getSessionIdByCookie(request);
+
+        // 2. 세션 ID 디코딩
+        if (sessionIdByCookie == null) {
+            log.warn("Session Cookie not found");
+            throw new AuthException.FailAuthenticationMemberException();
+        }
+        final String decodedSessionId = new String(Base64.getDecoder().decode(sessionIdByCookie.getBytes()));
+
+        // 3. Redis에서 세션 확인
+        if (!redisTemplate.hasKey(namespace + REDIS_SESSION_KEY + decodedSessionId)) {
+            log.warn("Session Cookie exists, but Session in Redis not found");
+            throw new AuthException.FailAuthenticationMemberException();
+        }
+
+        log.info("Valid Session: {}", decodedSessionId);
+        return true;
+    }
+
+    // 요청에서 세션 쿠키 추출
+    private String getSessionIdByCookie(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (var cookie : request.getCookies()) {
+                if (SESSION_KEY.equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+}
