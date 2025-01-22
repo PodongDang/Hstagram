b6191d175af13ed12baa046898397c053f6c5758
Local_Hwang <amongzamong@gmail.com>
2025-01-23 01:22:23 +0900

    feat : Add trust-store and password for connection

--- Hstagram/src/main/java/SNS/Hstagram/config/ElasticsearchConfig.java
+++ Hstagram/src/main/java/SNS/Hstagram/config/ElasticsearchConfig.java
@@ -7,12 +7,17 @@ import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+import javax.net.ssl.SSLContext;
+import java.io.FileInputStream;
+import java.security.KeyStore;
 
 @Configuration
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
@@ -25,13 +30,41 @@ public class ElasticsearchConfig extends ElasticsearchConfiguration {
     @Value("${spring.data.elasticsearch.url}")
     private String[] esHost;
 
+    @Value("${spring.data.elasticsearch.ssl.trust-store}")
+    private String trustStorePath;
+
+    @Value("${spring.data.elasticsearch.ssl.trust-store-password}")
+    private String trustStorePassword;
+
+
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
                 .connectedTo(esHost)
+                .usingSsl(createSSLContext())  // HTTPS 사용 설정
                 .withBasicAuth(username, password)
                 .build();
     }
+
+
+    private SSLContext createSSLContext() {
+        try {
+            String trustStorePath = this.trustStorePath; // truststore 경로
+            String trustStorePassword = this.trustStorePassword; // truststore 비밀번호
+
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            try (FileInputStream fis = new FileInputStream(trustStorePath)) {
+                trustStore.load(fis, trustStorePassword.toCharArray());
+            }
+
+            return SSLContextBuilder.create()
+                    .loadTrustMaterial(trustStore, null)
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create SSLContext", e);
+        }
+    }
+
 
 //    @Bean
 //    public ElasticsearchClient elasticsearchClient(RestClient restClient) {
