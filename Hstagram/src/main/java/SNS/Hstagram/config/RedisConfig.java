b59340f732292fda38759f0f4ca3ce88b5af1313
Local_Hwang <amongzamong@gmail.com>
2025-02-12 10:53:17 +0900

    feat : Add redisMessageListener for chatting service

--- Hstagram/src/main/java/SNS/Hstagram/config/RedisConfig.java
+++ Hstagram/src/main/java/SNS/Hstagram/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.*;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
@@ -54,6 +56,31 @@ public class RedisConfig {
 
         template.afterPropertiesSet();
         return template;
+    }
+
+    /**
+     * 메시지를 수신하고 처리할 수 있게 해주는 컨테이너
+     * @param connectionFactory RedisConnectionFactory
+     * @return RedisMessageListenerContainer
+     */
+    @Bean
+    public RedisMessageListenerContainer redisMessageListener(
+            RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        return container;
+    }
+
+    /**
+     * Redis에 대한 기본적인 연결과 통신을 담당하는 클래스
+     * @param redisConnectionFactory RedisConnectionFactory
+     * @return StringRedisTemplate
+     */
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        return new StringRedisTemplate(redisConnectionFactory);
     }
 
 
