99bda7c434dd5e48409cfa7488511fc5cd6ecc2a
Local_Hwang <amongzamong@gmail.com>
2025-02-12 10:53:29 +0900

    Create KafkaProducerConfig.java

---
+++ Hstagram/src/main/java/SNS/Hstagram/config/kafka/KafkaProducerConfig.java
@@ -0,0 +1,31 @@
+package SNS.Hstagram.config.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() { // ✅ 타입 수정: Object → String
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() { // ✅ 타입 수정: Object → String
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
